1157f5aa2488715cedd43c10bdc83225ffd64f65
suyaljagrati3 <suyaljagrati3@gmail.com>
2026-06-06 15:46:35 +0530

    Day-06-missing_element

--- Day-05-Move-Zeroes/linear.java
+++ Day-05-Move-Zeroes/linear.java
@@ -1,11 +1,4 @@
-class Solution {
-    public int linearSearch(int nums[], int target) {
-		for(int i=0; i<nums.length; i++) {
-            if(nums[i] == target) {
-                return i;
-            }
-        }
-        return -1;
-    }
 
-}
+
+
+
